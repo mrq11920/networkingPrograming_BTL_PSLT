baf0f8aa8e2a57f87238f121116ed4294e191d12
tunc2112 <sueruan012@gmail.com>
2021-12-17 00:09:12 +0700

    upload subscriber

---
+++ Subscriber/Subscriber.java
@@ -0,0 +1,100 @@
+/*
+* @author tunc2112
+* @group INT3304 1 - PSLT
+* Chương trình hiển thị thông tin cảm biến A đóng vai trò là Subscriber (bên đăng ký nhận thông tin)
+*/
+
+import java.net.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.regex.*;
+import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+
+public class Subscriber {
+	private static final String COMMAND_QUIT = "QUIT";
+
+	private Socket socket           = null;
+	private DataInputStream  input  = null;
+	private DataOutputStream output = null;
+
+	public Subscriber(String address, int port) {
+		try {
+			System.out.println("[+]Subscriber Socket Created!");
+			socket = new Socket(address, port);
+			System.out.println("[+]Connected to Broker\n" + getHelp());
+
+			input  = new DataInputStream(socket.getInputStream());
+			output = new DataOutputStream(socket.getOutputStream());
+		}
+		catch (UnknownHostException e) {
+			System.out.println(e);
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+
+		Scanner stdin = new Scanner(System.in);
+		while (true) {
+			try {
+				System.out.print("Subscriber: ");
+				System.out.flush();
+
+				String line = stdin.nextLine();
+				System.out.println("Input: " + line);
+				output.write(line.getBytes());
+				int count = input.available();
+				byte[] response_bytes = new byte[count];
+				int read_bytes = input.read(response_bytes);
+				String response = new String(response_bytes, StandardCharsets.UTF_8);
+				if (response.startsWith("{\"available_topics\":") && response.endsWith("\"}")) {
+					System.out.println("Broker: " + getAvailableTopics(response));
+				} else {
+					System.out.println("Broker: " + response);
+				}
+				if (line.equals(COMMAND_QUIT)) {
+					break;
+				}
+			}
+			catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+
+		// close the connection
+		try {
+			input.close();
+			output.close();
+			socket.close();
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+
+	public static String getAvailableTopics(String data) {
+		if (!data.startsWith("{\"available_topics\":") || !data.endsWith("\"}")) {
+			return "";
+		}
+		String[] topics = data.substring(21, data.length()-2).split(",");
+		String result = "Available topics:";
+		for (String topic: topics) {
+			result += "\n- " + topic;
+		}
+		return result;
+	}
+
+	public static String getHelp() {
+		return
+			"Note: “QUIT” to quit\n" + 
+			"Input “TOPIC” to subscribe an available topic\n" + 
+			"Input “SUB” to view all available topic then input a topic to subscribe to it\n" + 
+			"Input “LIST” to view all subscribed topics";
+	}
+
+	public static void main(String[] args) {
+		String ip_address = args.length > 0 ? args[0] : "127.0.0.1";
+		Subscriber Subscriber = new Subscriber(ip_address, 9111);
+	}
+}
